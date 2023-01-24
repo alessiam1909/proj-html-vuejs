3da670c5a6324ee7bcd16dd55e796bceb9743b9f
alessiam1909 <ale.menon.vr@gmail.com>
2023-01-24 11:31:01 +0100

    Aggiunta cartelle scss e installazione, inizio header

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,50 @@
-<script setup>
+<script >
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
 
+export default{
+  components: {
+    AppHeader,
+    AppFooter
+  }, 
+  data(){
+    return{
+      navList: [
+        {
+          name: "HOME",
+          url: "#null"
+        },
+        {
+          name: "ABOUT",
+          url: "https://avada.theme-fusion.com/driving/about/"
+        },
+        {
+          name: "PRICES",
+          url: "https://avada.theme-fusion.com/driving/prices/"
+        },
+        {
+          name: "COURSES",
+          url: "https://avada.theme-fusion.com/driving/courses/"
+        },
+        {
+          name: "LOCATIONS",
+          url: "https://avada.theme-fusion.com/driving/locations/"
+        },
+        {
+          name: "BLOG",
+          url: "https://avada.theme-fusion.com/driving/blog/"
+        }
+      ]
+    }
+  }
+}
 </script>
 
 <template>
-
-  
+<AppHeader :navList="navList"/>
+<AppFooter/>
 </template>
 
-<style scoped>
-
+<style lang="scss">
+@use './styles/generals.scss' as *
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+    props: {
+        navList: Array
+    }
+}
+</script>
+
+<template lang="">
+    <div class="first header">
+        <div class="row">
+            <div>We have a 95% Successful Pass Rate!</div>
+            <div>Give us a call to book your tuition! 1-800-555-555</div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+    @use '../styles/partials/variables.scss' as *;
+    @use '../styles/partials/mixins.scss' as *;
+
+    .first-header{
+        background-color: $black;
+
+        .row{
+            @include width80;
+        }
+    }
+</style>
